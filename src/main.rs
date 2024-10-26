3cc1d2690017859f103d733e2fa5a087a1742afd
David Richey <darichey1@gmail.com>
2024-10-26 17:52:51 -0500

    Add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 use std::{thread, time::Duration};
 
+use clap::Parser;
 use hidapi::HidApi;
 
+#[derive(clap::Parser, Debug)]
+struct Args {
+    #[clap(long, value_enum)]
+    switch_to: InputSource,
+
+    #[clap(long, default_value_t = 0x1462)]
+    vendor_id: u16,
+
+    #[clap(long, default_value_t = 0x3FA4)]
+    product_id: u16,
+}
+
+#[derive(clap::ValueEnum, Clone, Copy, Debug)]
+enum InputSource {
+    Dp = 0x32,
+    UsbC = 0x33,
+}
+
 fn main() {
+    let args = Args::parse();
     let api = HidApi::new_without_enumerate().unwrap();
-    let (vid, pid) = (0x1462, 0x3FA4);
 
     loop {
-        let Ok(device) = api.open(vid, pid) else {
-            println!("Device not found");
-            thread::sleep(Duration::from_secs(1));
-            continue;
+        let device = match api.open(args.vendor_id, args.product_id) {
+            Ok(device) => device,
+            Err(err) => {
+                println!("Got error opening device: {:?}", err);
+                thread::sleep(Duration::from_secs(1));
+                continue;
+            }
         };
 
         loop {
@@ -49,7 +71,7 @@ fn main() {
                 buf[7] = 0x30;
                 buf[8] = 0x30; //
                 buf[9] = 0x30; // -- theory: these three bytes encode the display in ascii: 003 for Usbc
-                buf[10] = 0x33; //
+                buf[10] = args.switch_to as u8; //
                 buf[11] = 0x0d; // footer
 
                 if let Err(e) = device.write(&buf) {
